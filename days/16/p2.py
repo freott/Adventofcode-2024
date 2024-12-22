945596da6527c7e75f235a499db16103939e3bb3
Fredrik Ottosson <fredrik.ottosson@positiongreen.com>
2024-12-22 20:52:55 +0100

    Finish day 16 part

--- days/16/p2.py
+++ days/16/p2.py
@@ -1,83 +1,79 @@
 from collections import deque
-from pprint import pprint
-from typing import List
+from functools import lru_cache
+from typing import List, Tuple
+
+grid: List[List[str]] = None
+start: Tuple[int, int] = None
+end: Tuple[int, int] = None
 
 def read(data: str):
+  global grid, start, end
   grid_rows = data.splitlines()
   grid = [['' for _ in range(len(grid_rows[0]))] for _ in range(len(grid_rows))]
-  visited_grid = [[float('inf') for _ in range(len(grid_rows[0]))] for _ in range(len(grid_rows))]
-  start = None
-  end = None
   for y, row in enumerate(grid_rows):
     for x, cell in enumerate(row):
       grid[y][x] = cell
       if cell == 'S': start = (y, x)
       if cell == 'E': end = (y, x)
-  return grid, visited_grid, start, end
 
 dd = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-
-def get_pos (pos: List[int], dir: int):
-  return (pos[0] + dd[dir][0], pos[1] + dd[dir][1])
-
 allowed = ['.', 'E', 'S']
-def get_paths(grid: List[List[int]], start_pos: List[int], start_dir: int):
-    results = []
-    for dir, score in [
-      [start_dir % 4, 1], 
-      [(start_dir - 1) % 4, 1001], 
-      [(start_dir + 1) % 4, 1001]
-    ]:
-      pos = get_pos(start_pos, dir)
-      if grid[pos[0]][pos[1]] in allowed:
-        results.append([pos, score, dir])
-    
-    return results
 
-def run(data: str, *args) -> int:
-  grid, visited_grid, start, end = read(data)
-  visited_grid2 = visited_grid.copy()
+@lru_cache(None)
+def get_paths(pos: Tuple[int, int], dir: int):
+  results = []
+  for d, score in [
+    [dir % 4, 1], 
+    [(dir - 1) % 4, 1001], 
+    [(dir + 1) % 4, 1001]
+  ]:
+    y, x = (pos[0] + dd[d][0], pos[1] + dd[d][1])
+    if grid[y][x] in allowed:
+      results.append(((y, x), d, score))
   
-  queue = deque([(start, 1, 0)])
+  return results
+  
+@lru_cache(None)
+def resolve_path(start: Tuple[int, int], start_dir: int):
+  queue = deque([(start, start_dir, 0)])
   best_score = float('inf')
+  seen = {}
 
   while queue:
     pos, dir, score = queue.popleft()
-    if score > best_score:
-      continue
+    if score > best_score: continue
     if pos == end:
       best_score = min(best_score, score)
       continue
-    if score >= visited_grid[pos[0]][pos[1]]:
+    if pos in seen and score >= seen[pos]:
       continue
+    seen.setdefault(pos, score)
     
-    visited_grid[pos[0]][pos[1]] = score
-    
-    for p, s, d in get_paths(grid, pos, dir):
+    for p, d, s in get_paths(pos, dir):
       queue.append((p, d, score + s))
-      
-  print(best_score)
-      
-  queue2 = deque([(end, 2, best_score, { end })])
-  print(end)
-  trails = set()
-  while queue2:
-    pos, dir, score, trail = queue2.popleft()
-    if score < 0:
-      continue
-    if pos == start:
-      trails = trails | trail
-      print('Found one')
-      continue
-    # if score < visited_grid2[pos[0]][pos[1]]:
-    #   continue
     
-    # visited_grid[pos[0]][pos[1]] = score
+  return best_score
+
+def run(data: str, *args) -> int:
+  read(data)
+  best_score = resolve_path(start, 1)
+  
+  best_tiles = { start }
+  iteratives = [(start, 1, 0, set())]
+  while(iteratives):
+    i = iteratives.pop(0)
+    pos, dir, score, seen = i
+    pos_dir = (pos, dir)
+    if pos_dir in seen: continue
+    seen.add(pos_dir)
     
-    for p, s, d in get_paths(grid, pos, dir):
-      new_trail = trail.copy()
-      new_score = score - s
-      new_trail.add(pos)
-      queue2.append((p, d, new_score, new_trail))
-  print(trails)
-  return len(trails)
+    paths = get_paths(pos, dir)
+    if not paths: continue
+    
+    for p, d, s in paths:
+      new_score = score + s
+      if resolve_path(p, d) + new_score != best_score: continue
+      best_tiles.add(p)
+      iteratives.append((p, d, new_score, seen.copy()))
+      
+  return len(best_tiles)
